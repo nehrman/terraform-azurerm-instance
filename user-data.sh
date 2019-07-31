8824c6aaba973027a6311690e6952b7311ad9034
nehrman <nicolas.ehrman@gmail.com>
2019-07-31 15:21:14 +0200

    Adding hostname in user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+HOST=`hostname`
 cat > index.html <<EOF
 <h1>Hello, World.</h1>
 <p>Brought to you by Terraform Automation</p>
-<p> Hosted on server $HOSTNAME</p>
+<p> Hosted on server $HOST</p>
 EOF
 
 nohup busybox httpd -f -p "80" &
